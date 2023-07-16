b19d8b640bbd704379900c9a19a5192337b8888a
German Popov <ProgDefv@gmail.com>
2023-07-16 20:15:16 +0400

    init app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+import { Person } from "./Person";
+import { Worker } from "./Worker";
+
+const user = new Worker("tom", 18, "OOO ZAVOD");
+const user2 = new Person("bob", 32);
+
+
+console.log(user);
+console.log(user2);
